44702f45cf490362ce7c956040d06eab42d21756
Borna Bešić <besic.borna@gmail.com>
2018-05-08 20:14:44 +0200

    Add dataset class skeleton

---
+++ visual-localization/dataset.py
@@ -0,0 +1,15 @@
+
+from torch.utils.data import Dataset
+
+class DeepLoc(Dataset):
+    def __init__(self):
+        # TODO
+        pass
+
+    def __len__(self):
+        # TODO
+        return 0
+
+    def __getitem__(self, idx):
+        # TODO
+        return None
