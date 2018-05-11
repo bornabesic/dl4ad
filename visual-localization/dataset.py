498e0ceb7a519fcd814fb094214060e112c88baf
Borna Bešić <besic.borna@gmail.com>
2018-05-11 18:29:02 +0200

    Refactor the preprocessing phase

--- visual-localization/dataset.py
+++ visual-localization/dataset.py
@@ -4,12 +4,14 @@ from torch.utils.data import Dataset
 from PIL import Image
 
 from utils import read_table
+from preprocessing import default_preprocessing
 
 class DeepLoc(Dataset):
 
-    def __init__(self, mode):
+    def __init__(self, mode, preprocess = default_preprocessing):
         self.data = []
         self.size = 0
+        self.preprocess = preprocess
 
         if mode not in ("train", "test"):
             raise ValueError("Only 'train' and 'test' modes are available.")
@@ -33,4 +35,6 @@ class DeepLoc(Dataset):
     def __getitem__(self, idx):
         image_path, pose = self.data[idx]
         image = Image.open(image_path)
+        if self.preprocess is not None:
+            image = self.preprocess(image)
         return (image, pose)
--- visual-localization/preprocessing.py
+++ visual-localization/preprocessing.py
@@ -1,25 +1,8 @@
 
-from torchvision.transforms import RandomCrop, Resize
+from torchvision.transforms import RandomCrop, Resize, Compose, ToTensor
 
-# Rescale the image so that the smaller edge of the image
-# is matched to 'smaller_size'.
-class RescaleSmaller:
-
-    def __init__(self, smaller_size):
-        self.transform = Resize(smaller_size)
-
-    def __call__(self, image):
-        return self.transform(image)
-
-# Extract ’number’ random crops of size ’width’ x ’height’.
-class RandomCrops:
-
-    def __init__(self, width, height, number):
-        self.number = number
-        self.transform = RandomCrop(size = (height, width))
-
-    def __call__(self, image):
-        crops = []
-        for _ in range(self.number):
-            crops.append(self.transform(image))
-        return crops
+default_preprocessing = Compose([
+    Resize(256), # Rescale so that the smaller edge is 256 pxs
+    RandomCrop(size = (224, 224)), # Take a random 224 x 224 crop
+    ToTensor()
+])
