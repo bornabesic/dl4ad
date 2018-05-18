b255adc39cfa8b961223045ae86c3f4bc103d349
Borna Bešić <besic.borna@gmail.com>
2018-05-18 18:15:56 +0200

    Update train.py

--- visual-localization/train.py
+++ visual-localization/train.py
@@ -5,9 +5,17 @@ import torch
 from dataset import DeepLocAugmented, make_train_valid_loader, make_test_loader
 from network import GoogLeNet
 
+# Load the dataset
 train_data = DeepLocAugmented("train")
 print("Training set size: {} samples".format(len(train_data)))
 
+# Generate the data loaders
 train_loader, valid_loader = make_train_valid_loader(train_data, valid_percentage = 0.2)
 
+# Define the model
 net = GoogLeNet()
+net.cuda()
+
+# Training phase
+for images, xs, qs in train_loader:
+    ps = torch.cat([xs, qs], dim = 1)
