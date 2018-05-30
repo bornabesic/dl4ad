574ad9096722aaef795f57c3cfcd6ef49d3cbf59
Borna Bešić <besic.borna@gmail.com>
2018-05-30 23:06:44 +0200

    Modify inception module and GoogLeNet

--- visual-localization/modules.py
+++ visual-localization/modules.py
@@ -1,49 +1,64 @@
 import torch
 import torch.nn as nn
 
-# Taken from:
+# Modified implementation originally taken from:
 # https://github.com/kuangliu/pytorch-cifar/blob/master/models/googlenet.py
 
 class Inception(nn.Module):
 
-    def __init__(self, in_planes, n1x1, n3x3red, n3x3, n5x5red, n5x5, pool_planes):
+    def __init__(self,
+            in_channels,
+            # Branch 1
+            conv1x1_out_channels,
+            # Branch 2
+            conv3x3_in_channels,
+            conv3x3_out_channels,
+            # Branch 3
+            conv5x5_in_channels,
+            conv5x5_out_channels,
+            # Branch 4
+            maxpool3x3_out_channels,
+        ):
         super(Inception, self).__init__()
 
         # 1 x 1 conv branch
         self.branch1 = nn.Sequential(
-            nn.Conv2d(in_planes, n1x1, kernel_size = 1),
-            nn.BatchNorm2d(n1x1),
+            nn.Conv2d(in_channels, conv1x1_out_channels, kernel_size = 1),
+            # nn.BatchNorm2d(conv1x1_out_channels),
             nn.ReLU(True),
         )
 
         # 1 x 1 conv -> 3 x 3 conv branch
         self.branch2 = nn.Sequential(
-            nn.Conv2d(in_planes, n3x3red, kernel_size = 1),
-            nn.BatchNorm2d(n3x3red),
+            # 1 x 1
+            nn.Conv2d(in_channels, conv3x3_in_channels, kernel_size = 1),
+            nn.BatchNorm2d(conv3x3_in_channels),
             nn.ReLU(True),
-            nn.Conv2d(n3x3red, n3x3, kernel_size = 3, padding = 1),
-            nn.BatchNorm2d(n3x3),
+            # 3 x 3
+            nn.Conv2d(conv3x3_in_channels, conv3x3_out_channels, kernel_size = 3, padding = 1),
+            # nn.BatchNorm2d(conv3x3_out_channels),
             nn.ReLU(True),
         )
 
         # 1 x 1 conv -> 5 x 5 conv branch
         self.branch3 = nn.Sequential(
-            nn.Conv2d(in_planes, n5x5red, kernel_size = 1),
-            nn.BatchNorm2d(n5x5red),
+            # 1 x 1
+            nn.Conv2d(in_channels, conv5x5_in_channels, kernel_size = 1, padding = 1),
+            nn.BatchNorm2d(conv5x5_in_channels),
             nn.ReLU(True),
-            nn.Conv2d(n5x5red, n5x5, kernel_size = 3, padding = 1),
-            nn.BatchNorm2d(n5x5),
-            nn.ReLU(True),
-            nn.Conv2d(n5x5, n5x5, kernel_size = 3, padding = 1),
-            nn.BatchNorm2d(n5x5),
-            nn.ReLU(True),
+            # 5 x 5
+            nn.Conv2d(conv5x5_in_channels, conv5x5_out_channels, kernel_size = 5, padding = 1),
+            # nn.BatchNorm2d(conv5x5_out_channels),
+            nn.ReLU(True)
         )
 
         # 3 x 3 pool -> 1 x 1 conv branch
         self.branch4 = nn.Sequential(
-            nn.MaxPool2d(3, stride = 1, padding = 1),
-            nn.Conv2d(in_planes, pool_planes, kernel_size = 1),
-            nn.BatchNorm2d(pool_planes),
+            # pool
+            nn.MaxPool2d(kernel_size = 3, stride = 1, padding = 1),
+            # 1 x 1
+            nn.Conv2d(in_channels, maxpool3x3_out_channels, kernel_size = 1),
+            # nn.BatchNorm2d(maxpool3x3_out_channels),
             nn.ReLU(True),
         )
 
--- visual-localization/network.py
+++ visual-localization/network.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 from modules import Inception
 
-# Taken from:
+# Modified implementation originally taken from:
 # https://github.com/kuangliu/pytorch-cifar/blob/master/models/googlenet.py
 
 class GoogLeNet(nn.Module):
@@ -12,42 +12,105 @@ class GoogLeNet(nn.Module):
         super(GoogLeNet, self).__init__()
 
         self.pre_layers = nn.Sequential(
-            nn.Conv2d(3, 192, kernel_size = 3, padding = 1),
-            nn.BatchNorm2d(192),
+            nn.Conv2d(in_channels = 3, out_channels = 64, kernel_size = 7, stride = 2),
+            nn.MaxPool2d(kernel_size = 3, stride = 2),
+            nn.ReLU(True),
+            #
+            nn.Conv2d(in_channels = 64, out_channels = 192, kernel_size = 3, stride = 1),
+            nn.MaxPool2d(kernel_size = 3, stride = 2),
             nn.ReLU(True),
         )
 
-        self.a3 = Inception(192,  64,  96, 128, 16, 32, 32)
-        self.b3 = Inception(256, 128, 128, 192, 32, 96, 64)
+        self.a3 = Inception(
+            in_channels = 192,
+            conv1x1_out_channels = 64,
+            conv3x3_in_channels = 96, conv3x3_out_channels = 128,
+            conv5x5_in_channels = 16, conv5x5_out_channels = 32,
+            maxpool3x3_out_channels = 32,
+        )
+            
+        self.b3 = Inception(
+            in_channels = 256,
+            conv1x1_out_channels = 128,
+            conv3x3_in_channels = 128, conv3x3_out_channels = 192,
+            conv5x5_in_channels = 32, conv5x5_out_channels = 96,
+            maxpool3x3_out_channels = 64,
+        )
 
-        self.maxpool = nn.MaxPool2d(3, stride = 2, padding = 1)
+        self.a4 = Inception(
+            in_channels = 480,
+            conv1x1_out_channels = 192,
+            conv3x3_in_channels = 96, conv3x3_out_channels = 208,
+            conv5x5_in_channels = 16, conv5x5_out_channels = 48,
+            maxpool3x3_out_channels = 64
+        )
+        self.b4 = Inception(
+            in_channels = 512,
+            conv1x1_out_channels = 160,
+            conv3x3_in_channels = 112, conv3x3_out_channels = 224,
+            conv5x5_in_channels = 24, conv5x5_out_channels = 64,
+            maxpool3x3_out_channels = 64
+        )
 
-        self.a4 = Inception(480, 192,  96, 208, 16,  48,  64)
-        self.b4 = Inception(512, 160, 112, 224, 24,  64,  64)
-        self.c4 = Inception(512, 128, 128, 256, 24,  64,  64)
-        self.d4 = Inception(512, 112, 144, 288, 32,  64,  64)
-        self.e4 = Inception(528, 256, 160, 320, 32, 128, 128)
+        self.c4 = Inception(
+            in_channels = 512,
+            conv1x1_out_channels = 128,
+            conv3x3_in_channels = 128, conv3x3_out_channels = 256,
+            conv5x5_in_channels = 24,  conv5x5_out_channels = 64,
+            maxpool3x3_out_channels = 64
+        )
+        self.d4 = Inception(
+            in_channels = 512,
+            conv1x1_out_channels = 112,
+            conv3x3_in_channels = 144, conv3x3_out_channels = 288,
+            conv5x5_in_channels = 32, conv5x5_out_channels = 64,
+            maxpool3x3_out_channels = 64
+        )
+        self.e4 = Inception(
+            in_channels = 528,
+            conv1x1_out_channels = 256,
+            conv3x3_in_channels = 160, conv3x3_out_channels = 320,
+            conv5x5_in_channels = 32, conv5x5_out_channels = 128,
+            maxpool3x3_out_channels = 128
+        )
 
-        self.a5 = Inception(832, 256, 160, 320, 32, 128, 128)
-        self.b5 = Inception(832, 384, 192, 384, 48, 128, 128)
+        self.a5 = Inception(
+            in_channels = 832,
+            conv1x1_out_channels = 256,
+            conv3x3_in_channels = 160, conv3x3_out_channels = 320,
+            conv5x5_in_channels = 32, conv5x5_out_channels = 128,
+            maxpool3x3_out_channels = 128
+        )
+        self.b5 = Inception(
+            in_channels = 832,
+            conv1x1_out_channels = 384,
+            conv3x3_in_channels = 192, conv3x3_out_channels = 384,
+            conv5x5_in_channels = 48, conv5x5_out_channels = 128,
+            maxpool3x3_out_channels = 128
+        )
 
-        self.avgpool = nn.AvgPool2d(8, stride = 1)
+        self.maxpool = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
+        self.avgpool = nn.AvgPool2d(kernel_size = 7, stride = 1)
         self.linear = nn.Linear(1024, 7)
 
     def forward(self, x):
         out = self.pre_layers(x)
+
         out = self.a3(out)
         out = self.b3(out)
         out = self.maxpool(out)
+
         out = self.a4(out)
         out = self.b4(out)
         out = self.c4(out)
         out = self.d4(out)
         out = self.e4(out)
         out = self.maxpool(out)
+
         out = self.a5(out)
         out = self.b5(out)
         out = self.avgpool(out)
+
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
