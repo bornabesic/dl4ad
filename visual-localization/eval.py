9eedc2c2c46d2fa3dd4c2af39a95bac49b6c4087
Borna Bešić <besic.borna@gmail.com>
2018-06-09 10:40:56 +0200

    Fix eval.py to be more abstract

--- visual-localization/eval.py
+++ visual-localization/eval.py
@@ -3,10 +3,9 @@
 import torch
 import argparse
 
-from dataset import DeepLocAugmented, make_loader, evaluate, evaluate_median
-from network import PoseNetSimple
+from dataset import DeepLocAugmented, make_loader, evaluate_median
+import network
 from customized_loss import Customized_Loss
-from preprocessing import validation_preprocessing
 
 # Parse CLI arguments
 args_parser = argparse.ArgumentParser(
@@ -17,6 +16,12 @@ args_parser.add_argument(
 	"model_path",
 	type = str,
 	help = "Path to the saved model parameters file"
+)
+
+args_parser.add_argument(
+	"architecture",
+	type = str,
+	help = "Neural network architecture to use",
 )
 
 args_parser.add_argument(
@@ -31,6 +36,7 @@ args = args_parser.parse_args()
 # Parameters
 LOSS_BETA = int(args.model_path.split("_")[1])
 MODE = args.mode
+ARCHITECTURE = args.architecture
 print("Beta: {}".format(LOSS_BETA))
 
 # Device - use CPU is CUDA is not available
@@ -40,23 +46,19 @@ else:
     device = torch.device("cpu")
 
 # Load the saved parameters
-net = PoseNetSimple()
+arch_class = getattr(network, ARCHITECTURE)
+net = arch_class()
 net.load_state_dict(torch.load(args.model_path))
 net.to(device = device)
 
 # Dataset
-data = DeepLocAugmented(MODE, preprocess = validation_preprocessing)
-print("{} samples: {}".format(MODE, len(data)))
-
-# Loader
-loader = make_loader(data, batch_size = 1)
+data = DeepLocAugmented(MODE, preprocess = None)
+print("{} samples: {}".format(MODE.title(), len(data)))
 
 # Loss function
 criterion = Customized_Loss(beta = LOSS_BETA)
 
 # Evaluate on the data
-print("[{}]".format(MODE))
-avg_loss = evaluate(net, criterion, loader, device)
-print("Average {} loss: {}".format(MODE, avg_loss))
-x_error_median, q_error_median = evaluate_median(net, loader, device)
+x_error_median, q_error_median, loss_median = evaluate_median(net, criterion, data, device)
 print("Median {} error: {:.2f} m, {:.2f} °".format(MODE, x_error_median, q_error_median))
+print("Median {} loss: {}".format(MODE, loss_median))
