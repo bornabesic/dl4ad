34b78e68d4acff6c5dad08730b0b15d26292ef3b
Borna Bešić <besic.borna@gmail.com>
2018-06-25 15:54:29 +0200

    Remove the random cropping from the training preprocessing

--- visual-localization/dataset.py
+++ visual-localization/dataset.py
@@ -79,8 +79,7 @@ class DeepLocAugmented(DeepLoc):
 class PerceptionCarDataset(Dataset):
 
     default_preprocessing = Compose([
-        Resize(256), # Rescale so that the smaller edge is 256 pxs
-        RandomCrop(size = (224, 224)), # Take a random 224 x 224 crop
+        Resize(size = (224, 224)), # Rescale so that the smaller edge is 256 pxs
         SubtractMean(),
         ToTensor()
     ])
