Corrected some mistakes during normalization

visual-localization/customized_loss.py
@@ -9,13 +9,15 @@ class Customized_Loss(nn.Module):
         self.beta = beta
 
     def forward(self, output, target):
-        Pos = target[:, :2] * 350
-        #Hardcoded re-normalization
+        Pos = target[:, :2]
+        Pos = Pos * 350
         Quat = target[:, 2:]
-        Pos_est = output[:,:2] * 350
+        Pos_est = output[:,:2]
+        Pos_est = Pos_est * 350
         Quat_est = output[:,2:]
         Quat_est_normalized = F.normalize(Quat_est)
         Pos_error = torch.norm(Pos_est - Pos, 2, dim = 1)
         Quat_error = torch.norm(Quat_est_normalized - Quat, dim = 1)
         total_error = torch.mean(Pos_error) + self.beta * torch.mean(Quat_error)
         return total_error
+

visual-localization/dataset.py
@@ -160,20 +160,20 @@ class PerceptionCarDataset(Dataset):
 
             # Randomly switch front three and back three images
             switch_front_and_back = np.random.choice((True, False)) if self.augment else False
-            if switch_front_and_back:
-                image_paths = image_paths[3:] + image_paths[:3]
-                a1, a2, a3 = euler_from_quaternion(q)
-                a3 = a3 + np.sign(a3) * np.pi # rotate the pose by 180°
-                qw, qx, qy, qz = quaternion_from_euler(a1, a2, a3)
+#            if switch_front_and_back:
+#                image_paths = image_paths[3:] + image_paths[:3]
+#                a1, a2, a3 = euler_from_quaternion(q)
+#                a3 = a3 + np.sign(a3) * np.pi # rotate the pose by 180°
+#                qw, qx, qy, qz = quaternion_from_euler(a1, a2, a3)
 
             images = map(Image.open, image_paths)
 
 
-            # Randomly augment the six images with one effect online
-            should_augment = np.random.choice((True, False)) if self.augment else False
-            if should_augment:
-                x = round(np.random.uniform(len(augmentations))) - 1
-                images = map(augmentations[x],images)
+#            # Randomly augment the six images with one effect online
+ #           should_augment = np.random.choice((True, False)) if self.augment else False
+ #           if should_augment:
+ #               x = round(np.random.uniform(len(augmentations))) - 1
+ #               images = map(augmentations[x],images)
 
             if self.preprocess is not None:
                 images = map(self.preprocess, images)
@@ -183,7 +183,11 @@ class PerceptionCarDataset(Dataset):
             image = torch.cat(images, dim = 0)
         
         # Hardcoded normalization to [-1,1]. Max. area from dataset is 350m x 350m
+   #     print("X vor teiler")
+  #      print(x)
         x = x / 350
+ #       print("X nach teiler")
+#        print(x)
         y = y / 350
         p = torch.Tensor([x, y, qw, qx, qy, qz])
 
@@ -203,7 +207,7 @@ class PerceptionCarDatasetMerged(Dataset):
         origin = reference + offset
         '''
         for dataset in self.datasets:
-            dataset.origin_offset = dataset.origin - self.origin
+            dataset.origin_offset = (dataset.origin - self.origin) / 350 #with hardcoded normalization
 
 
     def __getitem__(self, idx):
@@ -308,7 +312,9 @@ def evaluate_median(model, criterion, loader, device): # Expects a loader with b
         q = p[:, 2:].cpu().detach().numpy()
         x_out = p_out[:, :2].cpu().detach().numpy()
         q_out = p_out[:, 2:].cpu().detach().numpy()
-        
+        x = x *350
+        x_out = x_out *350
+
         error_x, theta = meters_and_degrees_error(x, q, x_out, q_out)
 
         x_errors.append(error_x)

visual-localization/train.py
@@ -9,7 +9,7 @@ import torch.optim as optim
 import matplotlib as mpl
 mpl.use("Agg")
 import matplotlib.pyplot as plt
-
+import numpy as np
 from dataset import DeepLocAugmented, make_loader, evaluate_median, PerceptionCarDataset, PerceptionCarDatasetMerged
 import network
 from network import parameters, PoseNetSimple
@@ -190,7 +190,6 @@ for epoch in range(EPOCHS):
         last_training_loss =  losses[-1].item()
         training_loss.append(last_training_loss)
         num_iters += 1
-        print("Loss of last training batch: {}".format(last_training_loss))
 
         # Do a backpropagation step
         optimizer.zero_grad()