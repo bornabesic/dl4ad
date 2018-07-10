bc8d72bf7416a1f658b1a5510c8276b0e4d05518
Borna Bešić <besic.borna@gmail.com>
2018-07-10 11:30:14 +0200

    Fix the loss function

--- visual-localization/customized_loss.py
+++ visual-localization/customized_loss.py
@@ -12,24 +12,20 @@ class Customized_Loss(nn.Module):
     def forward(self, output, target):
         # Ground truth
         xys = target[:, :2]
-        cosines = target[:, 2]
-        sines = target[:, 3]
+        cos_sin = target[:, 2:4]
 
         # Predictions
         xy_preds = output[:, :2]
-        cosines_preds = output[:, 2]
-        sines_preds = output[:, 3]
+        cos_sin_preds = output[:, 2:4]
+        cos_sin_preds = F.normalize(cos_sin_preds)
 
         # Position error
         xy_diff = xy_preds - xys
         xy_errors = torch.norm(xy_diff, p = 2, dim = 1)
 
         # Orientation error
-        cosines_diff = cosines_preds - cosines
-        cosines_errors = torch.norm(cosines_diff, p = 2)
-
-        sines_diff = sines_preds - sines
-        sines_errors = torch.norm(sines_diff, p = 2)
+        cos_sin_diff = cos_sin_preds - cos_sin
+        cos_sin_errors = torch.norm(cos_sin_diff, p = 2, dim = 1)
 
         ''' Assertions '''
         inf = float("inf")
@@ -37,11 +33,9 @@ class Customized_Loss(nn.Module):
         assert not torch.isnan(xy_errors).byte().any() # All elements in the tensor are zero (no NaNs)
         assert not (xy_errors == inf).byte().any() # All elements in the tensor are zero (no infs)
 
-        assert not torch.isnan(cosines_errors).byte().any() # All elements in the tensor are zero (no NaNs)
-        assert not (cosines_errors == inf).byte().any() # All elements in the tensor are zero (no infs)
+        assert not torch.isnan(cos_sin_errors).byte().any() # All elements in the tensor are zero (no NaNs)
+        assert not (cos_sin_errors == inf).byte().any() # All elements in the tensor are zero (no infs)
 
-        assert not torch.isnan(sines_errors).byte().any() # All elements in the tensor are zero (no NaNs)
-        assert not (sines_errors == inf).byte().any() # All elements in the tensor are zero (no infs)
         ''''''
 
         # theta0_errors = torch.norm(theta_preds - thetas, 2, dim = 1)
@@ -50,7 +44,7 @@ class Customized_Loss(nn.Module):
         # theta_errors, _ = torch.min(theta_errors_both, dim = 1)
 
         # total_error = self.beta * torch.mean(xy_errors) + torch.mean(theta_errors)
-        angle_error = self.beta * (torch.mean(cosines_errors) + torch.mean(sines_errors))
+        angle_error = self.beta * torch.mean(cos_sin_errors)
         position_error = torch.mean(xy_errors) 
         # print(position_error, angle_error)
         total_error = position_error + angle_error
